9391ebfc397e2845cb53052e45e679d50adbcd8a
handan05 <handan05@58.com>
2019-11-05 20:07:37 +0800

    test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,10 @@ import Cookie, {
   // delCookie,
 } from "../index.js";
 
-let newCookie = new Cookie() ;
+let newCookie = new Cookie({
+  time: 20,
+  path: '/'
+}) ;
 
 var setCookie = newCookie.setCookie ;
 var getCookie = newCookie.getCookie ;
@@ -15,17 +18,28 @@ var delCookie = newCookie.delCookie ;
 
 
 setCookie("login", "UID=596915259361234&UN=zhaopinqw01&TT=26bd8b06210ea3c301f3975sdf3fe")
+
+
 setCookie("name", "张三") ;
+console.log(getCookie("name")) ;
+
+
 setCookie("name2", "lisi", { time: 3 })
+console.log(getCookie("name2")) ;
+
+
 setCookie("name3", "wangwu", { time: 3 , path: "/app.html" }) ;
+console.log(getCookie("name3")) ;
+
+
 setCookie("name4", 'sdf', { time: 3, path: "/app.html", domain: "localhost" })
+console.log(getCookie("name4")) ;
+
 
 setCookie("name5", "abc", [1,2,3])
+console.log(getCookie("name5")) ;
 
-console.log(getCookie("name")) ;
-console.log(getCookie("name2")) ;
-console.log(getCookie("name3")) ;
-console.log(getCookie("name4")) ;
+
 
 console.log(getCookieName("login", "UID"))
 console.log(getCookieName("login", "UN"))
